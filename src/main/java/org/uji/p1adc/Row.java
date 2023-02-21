6ee588bbae117cde650f1a182175666cdef12179
Abel <al415746@uji.es>
2023-02-21 12:19:49 +0100

    Mas cosas (Por desgracia)

--- src/main/java/org/uji/p1adc/Row.java
+++ src/main/java/org/uji/p1adc/Row.java
@@ -1,9 +1,10 @@
 package org.uji.p1adc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Row {
-    List<Double> data;
+    List<Double> data =  new ArrayList<>();
         public List<Double> getData(){
             return data;
     }
--- src/main/java/org/uji/p1adc/Table.java
+++ src/main/java/org/uji/p1adc/Table.java
@@ -1,10 +1,12 @@
 package org.uji.p1adc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Table {
-    List<String> header;
-    public List<Double> getRowAt(int rowNumber){
+    List<String> header= new ArrayList<>();
+    List<Row> rows = new ArrayList<>();
+    public List<Double> getRowAt(Row rowNumber){
         return null;
     }
 }
